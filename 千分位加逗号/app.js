76cfbfea33cad6363dcc858b6d278655d3146160
yuanlinssdut <yuanlinssdut@gmail.com>
2017-08-01 10:01:17 +0800

    优化

--- 千分位加逗号/app.js
+++ 千分位加逗号/app.js
@@ -2,11 +2,12 @@ var num = '1234256.22233';
 var nStr0 = num.split('.')[0];
 var nStr1 = num.split('.')[1]
 var len = nStr0.length;
-var nStr2 = nStr0.split("").reverse().map(function (item, index) {
-  if ((index + 1) % 3 == 0 && index !== len - 1) {
+var nStr2 = nStr0.split("").map(function (item, index) {
+  var rIndex = len - index
+  if (rIndex % 3 == 0 && rIndex !== 0) {
     return ',' + item
   } else {
     return item
   }
-}).reverse().join("");
+}).join("");
 console.log(nStr2 + '.' + nStr1)
